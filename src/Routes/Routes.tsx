ed4870b1f989c146d530ba3f434d0cb68164bcc6
Vinayak jain <80898516+vinayakj001@users.noreply.github.com>
2023-05-18 23:10:52 +0530

    Update Routes.tsx

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -1,18 +1,18 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React, { useContext } from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { AppStateContext } from "../App";
+import NotFound from "../ExpeptionHandling/NotFound";
 import Login from "../Login/Login";
 import Dashboard from "../Pages/Dashboard";
-import NotFound from "../ExpeptionHandling/NotFound";
 
 const AppRouter: React.FC = () => {
-  const key = localStorage.getItem("authenticated");
+  const { myState } = useContext(AppStateContext);
+
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        {key === "72f4f34t4ytg44" && (
-          <Route path="/dashboard" element={<Dashboard />} />
-        )}
+        {myState && <Route path="/dashboard" element={<Dashboard />} />}
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
